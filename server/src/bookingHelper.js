5ef60a72dbaf04a222b97db6545bb3b2a00fc945
andrew-dev-p <popov.andrew.dev@gmail.com>
2025-06-04 17:36:00 +0300

    Add express-session for session management and implement booking functionality with Twilio SMS integration

---
+++ server/src/bookingHelper.js
@@ -0,0 +1,61 @@
+import dotenv from "dotenv";
+dotenv.config();
+
+const weekdays = process.env.WEEKDAYS.split(",");
+const hourRegex = /\b(\d?\d)\s?([aApP][mM])/g;
+
+function matchType(req, messageContent) {
+  if (messageContent.includes("gym")) {
+    req.session.type = "gym";
+  } else if (messageContent.includes("personal")) {
+    req.session.type = "personal trainer";
+  } else if (messageContent.includes("massage")) {
+    req.session.type = "masseur";
+  }
+
+  if (!req.session.type) {
+    return `Sorry I didn't undestand your request`;
+  }
+
+  req.session.step = 2;
+  return `What date do you want to see the ${req.session.type}`;
+}
+
+function matchDay(request, messageContent) {
+  const day = weekdays.filter((w) => messageContent.toLowerCase().includes(w));
+  if (day.length === 0) {
+    return "I'm not sure what day of the week do you want to make a booking for";
+  }
+  if (day.length > 1) {
+    return `Please select just one day for the booking do you prefer ${weekday.join(
+      ", "
+    )} `;
+  }
+  request.session.step = 3;
+  request.session.weekday = day[0];
+  return `Do you want to book it on ${day[0]}:\n
+          10am, 11am, 1pm, 4pm`;
+}
+
+function matchTime(request, messageContent) {
+  const match = hourRegex.exec(messageContent);
+  if (!match && match.length !== 3) {
+    return `Sorry I could not understand what time do you want to come to see the ${type} on ${weekday}`;
+  }
+  const { type, weekday } = request.session;
+  request.session.step = 4;
+  request.session.time = match[0];
+  return `Your appointment to see the ${type} on ${weekday} at ${match[0]} was made, please let us know if you need to change the time, otherwise see you than.`;
+}
+
+function confirmBooking(request) {
+  const { type, weekday, time } = request.session;
+  return `Your appointment is booked to see the ${type} on ${weekday} at ${time}. If you want to change it, please contact us at 555-5555`;
+}
+
+module.exports = {
+  matchType,
+  matchDay,
+  matchTime,
+  confirmBooking,
+};
--- server/src/main.js
+++ server/src/main.js
@@ -1,9 +1,20 @@
 import express from "express";
 import dotenv from "dotenv";
 import twilio from "twilio";
+import session from "express-session";
+import { MessagingResponse } from "twilio";
+import bookingHelper from "./bookingHelper.js";
 
 const app = express();
 dotenv.config();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET,
+  })
+);
 
 const client = twilio(
   process.env.TWILIO_TOKEN_SID,
@@ -27,6 +38,46 @@ const sendTestMessage = async () => {
       console.log(err);
     });
 };
+
+// called by a twilio webhook
+app.post("/receive-sms", (req, res) => {
+  const messageContent = req.body.Body.toLowerCase();
+
+  const step = req.session.step;
+
+  let message;
+
+  if (!step) {
+    req.session.step = 1;
+
+    message = `Hi, do you want to book an appointment to: \n
+    see the gym \n
+    book a personal trainer \n
+    book a massage`;
+  } else {
+    switch (step) {
+      case 1:
+        message = bookingHelper.matchType(req, messageContent);
+        break;
+      case 2:
+        message = bookingHelper.matchDay(req, messageContent);
+        break;
+      case 3:
+        message = bookingHelper.matchTime(req, messageContent);
+        break;
+      case 4:
+        message = bookingHelper.confirmBooking(req);
+      default:
+        console.log(`Could not find the step for value: ${step}`);
+    }
+  }
+
+  const twiml = new MessagingResponse();
+  twiml.message(message);
+
+  res.set("Content-Type", "text/xml");
+  res.send(twiml.toString());
+});
 
 app.listen(process.env.PORT, () => {
   console.log(`Server is running on port ${process.env.PORT}`);
